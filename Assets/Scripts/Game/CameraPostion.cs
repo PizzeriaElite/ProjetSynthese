8a602285bae0bb5761e7a62e86d6ddf32abf619e
Lord Catzorz <rafsyl@hotmail.com>
2014-10-20 11:49:49 -0400

    Mendant 4.1 et 4.3 terminé
    La caméra suit le joueur et la caméra possède une zone ou il ne se
    recentrera pas avec sa cible.

---
+++ Assets/Scripts/Game/CameraPostion.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPostion : MonoBehaviour 
+{
+	const float DEFAULT_SMOOTING_TIME = 0.2f;
+	public GameObject CameraTarget = null;
+	public float CameraDepth = -8f;
+	public float RadiusOfAcceptableCentre = 1f;
+	public float smoothingTime = DEFAULT_SMOOTING_TIME;
+	private Vector3 cameraVelocity = Vector3.zero;
+	private bool mustCentre = false;
+	
+
+	void Start ()
+	{
+		if (this.CameraTarget != null)
+		{
+			Camera.main.transform.position = new Vector3(this.CameraTarget.transform.position.x, this.CameraTarget.transform.position.y, this.CameraDepth );
+		}
+	}
+
+	void Update () 
+	{
+		if (this.CameraTarget != null)
+		{
+			// Set the centre of the camera on the target
+			if (Vector2.Distance(Camera.main.transform.position, this.CameraTarget.transform.position) > this.RadiusOfAcceptableCentre)
+			{
+				this.mustCentre = true;
+			}
+			if (Vector2.Distance (Camera.main.transform.position, CameraTarget.transform.position) == 0)
+			{
+				this.mustCentre = false;
+			}
+
+			if (this.mustCentre == true)
+			{
+				Debug.Log("Centering");
+				Vector3 centeringTargetPostion = new Vector3(this.CameraTarget.transform.position.x, this.CameraTarget.transform.position.y, this.CameraDepth );
+
+				Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, centeringTargetPostion, ref this.cameraVelocity, this.smoothingTime);
+			}
+		}
+	}
+}
